c91f6696a43ad06e18c129803d61de674f463eca
Przemyslaw Anton Arciszewski <przemyslawapl@gmail.com>
2023-10-17 19:36:28 +0200

    Aðlagaði myndir

--- Scripts/rat.gd
+++ Scripts/rat.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 100
+@export var speed = 500
 @onready var target = Vector2.ZERO#$main/player.global_position
 # Called when the node enters the scene tree for the first time.
 func _ready():
