b568269d1a278ff7594fa15214a89a79c67f6851
Przemyslaw Anton Arciszewski <przemyslawapl@gmail.com>
2023-10-17 19:30:35 +0200

    Bætti inn rottu

---
+++ Scripts/rat.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 100
+@onready var target = Vector2.ZERO#$main/player.global_position
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	
+	velocity = position.direction_to(target) * speed * delta
+	move_and_slide()
+	
+	
+
